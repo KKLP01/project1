0b16853c8ce5517e395404bb3c21d64d23ef3625
KimYeIn <wmfrjqsp@gmail.com>
2024-05-09 15:17:57 +0900

    2nd commit
    마이너스 기능과 테스트입니다.

--- src/main/java/com/project1/calc/Minus.java
+++ src/main/java/com/project1/calc/Minus.java
@@ -2,4 +2,7 @@ package com.project1.calc;
 
 public class Minus {
 
+    public int minusTwoNumbers(int a, int b) {
+        return a - b;
+    }
 }
---
+++ src/test/java/com/project1/MinusTests.java
@@ -0,0 +1,27 @@
+package com.project1;
+
+import com.project1.calc.Minus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MinusTests {
+
+    @Test
+    @DisplayName("6과 3을 전달하면 3이 나오는지 확인")
+    public void testMinusTowNumbers() {
+        Minus minus = new Minus();
+        int result = minus.minusTwoNumbers(6, 3);
+
+        Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    @DisplayName("10과 4를 전달하면 6이 나오는지 확인")
+    public void testMinusTowNumbers2() {
+        Minus minus = new Minus();
+        int result = minus.minusTwoNumbers(10, 4);
+
+        Assertions.assertEquals(4, result);
+    }
+}
