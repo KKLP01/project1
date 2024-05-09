b1ff3b527939519e36d7432e5b8851f823c176bc
KDY <lemon8818@naver.com>
2024-05-09 16:45:50 +0900

    곱하기 테스트코딩 완료

---
+++ src/test/java/com/project1/MultiTests.java
@@ -0,0 +1,20 @@
+package com.project1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MultiTests {
+
+
+    @Test
+    public void testMulti() {
+
+        Multi multi = new Multi();
+        int result = multi.multiNumbers(10, 55);
+
+        Assertions.assertEquals(55, result);
+    }
+
+
+}
+
