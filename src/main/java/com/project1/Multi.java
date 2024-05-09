594ec7f3a2b8c889e79a51c01a15acc7ab282f0d
KDY <lemon8818@naver.com>
2024-05-09 15:23:47 +0900

    곱하기 개발 완료

--- src/main/java/com/project1/Multi.java
+++ src/main/java/com/project1/Multi.java
@@ -1,6 +1,10 @@
 package com.project1;
 
 public class Multi {
+    public int multiNumbers(int a, int b){
 
+        System.out.println("a, b = " + a + ", b = " + b);
+        return a*b;
+    }
 
 }
